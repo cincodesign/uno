c2e5f02475af3aa8535cb78b3ab01a222dc58589
Cabe Branson <cabebranson@gmail.com>
2017-07-28 14:35:02 -0700

    add env to eslint 🙃

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,10 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'no-console': 0,
+    'no-unused-expressions': ['error', { allowTernary: true }],
+  },
+  env: {
+    browser: true,
+    node: true,
   },
 };
